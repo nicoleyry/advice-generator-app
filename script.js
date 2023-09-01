b575eb9ef31e61c8a4a4be4f4ac4594f3881befc
Nicole <yoyotpe@hotmail.com>
2023-09-01 16:18:43 +0800

    update js

--- script.js
+++ script.js
@@ -2,14 +2,16 @@ let adviceID = document.getElementById('advice-id');
 let adviceQuote = document.getElementById('advice-quote');
 let btn = document.getElementById('advice-btn');
 
-btn.addEventListener('click', () => {
+let getAdvice = () => {
 	axios({
 		method: 'get',
 		url: 'https://api.adviceslip.com/advice',
 	}).then(function (response) {
 		let data = response.data;
-		console.log(data.slip);
 		adviceID.innerHTML = data.slip.id;
 		adviceQuote.innerHTML = data.slip.advice;
 	});
-});
+};
+
+document.addEventListener('DOMContentLoaded', getAdvice);
+btn.addEventListener('click', getAdvice);
